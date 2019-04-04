023e1524f81cd5021419ddf8fbc260bbe42b2811
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2019-04-04 10:49:54 +0200

    merge med pipeserver

---
+++ server1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+        private void Run()
+        {
+            Server server = new Server();
+            server.Run("myPipe");
+        }
+    }
+}
---
+++ server1/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeServer
+{
+    class Server
+    {
+        private StreamReader pipeReader;
+        private bool running = true;
+        private string input = string.Empty;
+
+        public void Run(string pipeName)
+        {
+            Console.Write("Venter på Klient...");
+            using (NamedPipeServerStream server = new NamedPipeServerStream(pipeName))
+            {
+                //Wait for client to connect:
+                server.WaitForConnection();
+                Console.WriteLine(" - klient tilsluttet!");
+
+                using (pipeReader = new StreamReader(server))
+                {
+                    while (running)
+                    {
+                        Console.Write("Venter på input...");
+                        input = GetLineFromClient(pipeReader);
+                        running = ManageInput(input);
+                    }
+                }
+            }
+        }
+
+        private string GetLineFromClient(StreamReader reader)
+        {
+            string result = string.Empty;
+            while (reader.Peek() >= 0)
+            {
+                result += reader.ReadLine();
+            }
+            reader.DiscardBufferedData();
+            return result.ToUpper();
+        }
+
+        private bool ManageInput(string input)
+        {
+            Console.WriteLine("\nInput var: " + input);
+            return !(input.ToUpper().Equals("EXIT"));
+        }
+    }
+}
