16f0a111011f6229d8834843f66115e25f77c9e6
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2019-04-04 10:56:02 +0200

    tilføjet currency class

---
+++ server1/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeServer
+{
+    class Currency
+    {
+        public string CountryCode { get; set; }
+        public int ExchangeRate { get; set; }
+
+        public Currency(string countryCode, int exchangeRate)
+        {
+            CountryCode = countryCode;
+            ExchangeRate = exchangeRate;
+        }
+    }
+}
